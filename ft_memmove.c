1f7d792f1e298a7a40d1023e38aaa50e74386b26
Github <zakaria.addi@gmail.com>
2025-11-10 10:48:53 +0100

    update memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,16 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaddi <zaddi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 10:32:31 by zaddi             #+#    #+#             */
+/*   Updated: 2025/11/10 10:40:26 by zaddi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void    *ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    unsigned char buf[n];
+    size_t  i;
 
-    ft_memcpy(buf, src, n);
-    i = 0;
-    while(i < n)
+    if(src == dest)
+        return (dest);
+    else if (src > dest)
     {
-        ((char*) dest)[i] = ((char*) buf)[i];
-        i++;
+        i = 0;
+        while(i < n)
+        {
+            ((char*) dest)[i] = ((char*) src)[i];
+            i++;
+        }
+    }
+    else
+    {
+        i = n - 1;
+        while(i >=0)
+        {
+            ((char*) dest)[i] = ((char*) src)[i];
+            i--;
+        }
     }
     return (dest);
 }
